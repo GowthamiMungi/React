619d8f10afaeca0f91267b0d4653e21ce18c403e
Gowthami <gowthami_mungi@thbs.com>
2024-01-23 16:08:45 +0530

    Save

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import QuizHistory from './QuizHistory';
 import './App.css'; // Import your global CSS file
 import './QuizHistory.css'; // Import QuizHistory component-specific CSS
-
+// import Navbar from "./Navbar";
 
 function App() {
   const [activeTab, setActiveTab] = useState('quizHistory'); // Initial tab
@@ -30,4 +30,38 @@ function App() {
 
 
 export default App;
+
+
+
+
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import History from './History';
+// import './History.css'; // Import your CSS file
+// import Navbar from "./Navbar";
+// import { useState } from "react";
+// const App = () => {
+//   const [user, setUser] = useState("");
+//   const [userId, setUserId] = useState(0);
+//   return (
+ 
+//     <Router>
+//       <div className="app">
+//         <Navbar />
+//       <Routes>
+//       <Route
+//           path="/history"
+//           exact
+//           element={<History user={user} setUser={setUser} 
+//           userId = {userId}
+//           setUserId = {setUserId}/>}
+//         />
+
+//       </Routes>
+//       </div>
+
+//     </Router>
+//   );
+// };
+
+// export default App;
 
